b6879d16fc6f0dfb3ae9a918bcff67ac016d9e60
Ivan <serralta.ivan@gmail.com>
2023-10-19 21:11:50 -0300

    Exaple arr

--- ActivitiesOrganizer/main.py
+++ ActivitiesOrganizer/main.py
@@ -1,12 +1,15 @@
 from ActivitiesOrganizer.ChargeActivities.mainCharge import *
+from Prototypes.prototypes import Activity
 from Methods.mainMethods import *
 from PrintActivities.mainWrite import *
 
 
-def test():
-#    activities = mainCharge()
 
-    week=chargeWeek([Activity('Gym',2,3,'morning'),Activity('University',2,3,'morning')])
+def test():
+    exampleAct = [Activity('Gym',2,3,'morning'),Activity('University',2,3,'morning'),Activity('Football',2,3,'afterNoon'),Activity('Exam',1,2,'morning'),Activity('Meet',1,4,'afterNoon')]
+    #activities = mainCharge()
+
+    week=chargeWeek(exampleAct)
 
     writeWeek(week)
 
